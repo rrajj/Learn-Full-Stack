a1e4b69785e435d7869a294f3e033a64f19c3080
Rishav <rraj150997@gmail.com>
2023-07-26 05:49:24 -0500

    part1a completion

--- part1/src/App.js
+++ part1/src/App.js
@@ -37,6 +37,41 @@ const Hello = () => {
   )
 }
 
+// component with props
+const Greet = (props) => {
+  return (
+    <div>
+      <p>Greetings, {props.name}</p>
+    </div>
+  )
+}
+
+// multiple props
+const Props2 = (props) => {
+  // logging
+  console.log(props)
+  return(
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+// react comoponent names must be capitalized
+// NEEDS TO START WITH CAPITAL LETTER
+// footer -> wont work
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+
+// MAIN APP
 const App = () => {
   // to log in the console
   // console.log("Hello from Component")
@@ -46,13 +81,49 @@ const App = () => {
   const b = 20
   console.log(now, a + b)
 
+  const friends = [
+    {name: 'Peter', age: 5},
+    {name: 'Mia', age: 10}
+  ]
+
+  const strings = ["A", "B"]
+  
   return (
+    /*
+    removed <div> tag because it was extra element in DOM tree
+    used fragments i.e. wrapping the elements to be returned by the
+    the compponent with an empty element
     <div>
+    */
+    <>
       <h1>Greetings</h1>
       <Hello/>
       <Hello/>
-    </div>
+
+      {/* calling new component with props */}
+      <Greet name="George"/>
+      <Greet name="Daisy"/>
+
+      {/* calling new component with multiple props */}
+      <Props2 name="John" age="35"/>
+      <Props2 name="Jack" age={25+35}/>
+      <Props2 name="Ben" age={b}/>
+
+
+      {/* DONOT RENDER OBJECTS */}
+      <br></br>
+      <h3>Using as array of dicts kinda. Donot render objects</h3>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+
+      <br></br>
+      <h3>if array contains values that are eligible for rendering BUT result would be kinda weird</h3>
+      <p>{strings}</p>
+      <Footer/>
+    </>
+    // </div>
   )
+  
 
 
   // return HTML markup
